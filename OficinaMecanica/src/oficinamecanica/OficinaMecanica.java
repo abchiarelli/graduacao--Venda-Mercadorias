8b733e013fc20c5ec0995f734261efaa6f2a04f0
abchiarelli <121977175+abchiarelli@users.noreply.github.com>
2023-05-09 11:35:58 -0300

    Initial commit

---
+++ OficinaMecanica/src/oficinamecanica/OficinaMecanica.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oficinamecanica;
+
+import tela.FrmPrincipal;
+
+/**
+ *
+ * @author artur
+ */
+public class OficinaMecanica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new FrmPrincipal().setVisible(true);
+    }
+    
+}
