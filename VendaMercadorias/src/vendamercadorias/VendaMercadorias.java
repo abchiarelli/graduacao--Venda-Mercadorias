9f48a5ebd53f67bb983d96e8d623341d2a5efe76
abchiarelli <121977175+abchiarelli@users.noreply.github.com>
2023-05-11 15:09:24 -0300

    Criado Frame Principal

--- VendaMercadorias/src/vendamercadorias/VendaMercadorias.java
+++ VendaMercadorias/src/vendamercadorias/VendaMercadorias.java
@@ -4,6 +4,8 @@
  */
 package vendamercadorias;
 
+import tela.FrmPrincipal;
+
 /**
  *
  * @author artur
@@ -14,7 +16,7 @@ public class VendaMercadorias {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new FrmPrincipal().setVisible(true);
     }
     
 }
