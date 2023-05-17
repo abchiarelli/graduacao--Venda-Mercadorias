df6cb3d99034582462c00d8205d9bacbcc9c8dc4
abchiarelli <121977175+abchiarelli@users.noreply.github.com>
2023-05-17 19:56:53 -0300

    Refatoração metodo construtor Cidade

--- VendaMercadorias/src/entidade/Cidade.java
+++ VendaMercadorias/src/entidade/Cidade.java
@@ -12,6 +12,16 @@ public class Cidade {
     private int id;
     private String nome;
 
+    public Cidade(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+    
+    public Cidade(String nome) {
+        this.nome = nome;
+    }
+    
+    
     public int getId() {
         return id;
     }
