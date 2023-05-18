e2fe59bda7e0b8e620527407e76d55bcbe6ab0c1
abchiarelli <121977175+abchiarelli@users.noreply.github.com>
2023-05-17 21:22:46 -0300

    Criação da Entidade DAO Produto

---
+++ VendaMercadorias/src/dao/ProdutoDAO.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import apoio.ConexaoBD;
+import apoio.IDAOT;
+import entidade.Produto;
+import java.util.ArrayList;
+import java.sql.*;
+
+/**
+ *
+ * @author artur
+ */
+public class ProdutoDAO implements IDAOT<Produto> {
+
+    @Override
+    public String salvar(Produto o) {
+        
+        try {
+            String dml = "INSERT INTO produto VALUES ("
+                    + "DEFAULT, "
+                    + "'" + o.getDescricao() + "', "
+                    + o.getValor() + ", "
+                    + o.getQuantidade()
+                    + ");";
+            
+            Statement st = ConexaoBD.getInstancia().getConexao().createStatement();
+            
+            int retorno = st.executeUpdate(dml);
+            
+            return null;
+        } catch (Exception e) {
+            System.out.println("Erro: " + e);
+            return e.toString();
+        }
+    }
+
+    @Override
+    public String atualizar(Produto o) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String excluir(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ArrayList<Produto> consultarTodos() {
+        ArrayList<Produto> produtos = new ArrayList<>();
+        
+        try {
+            String dml = "SELECT * FROM produto ORDER BY descricao";
+            
+            Statement st = ConexaoBD.getInstancia().getConexao().createStatement();
+            
+            ResultSet rs = st.executeQuery(dml);
+            
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String descricao = rs.getString("descricao");
+                float valor = rs.getFloat("valor");
+                float quantidade = rs.getFloat("quantidade");
+                
+                Produto produto = new Produto(id, descricao, valor, quantidade);
+                
+                produtos.add(produto);
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao consultar PRODUTOS: " + e);
+        }
+        
+        return produtos;
+    }
+
+    @Override
+    public ArrayList<Produto> consultar(String criterio) {
+        ArrayList<Produto> produtos = new ArrayList<>();
+        
+        try {
+                        
+            Statement st = ConexaoBD.getInstancia().getConexao().createStatement();
+            
+            ResultSet rs = st.executeQuery(criterio);
+            
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String descricao = rs.getString("descricao");
+                float valor = rs.getFloat("valor");
+                float quantidade = rs.getFloat("quantidade");
+                
+                Produto produto = new Produto(id, descricao, valor, quantidade);
+                
+                produtos.add(produto);
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao consultar PRODUTOS: " + e);
+        }
+        
+        return produtos;
+    }
+
+    @Override
+    public Produto consultarId(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
