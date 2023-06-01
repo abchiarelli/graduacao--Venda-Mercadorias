cebe830489305238d652f1d0ffc6ef3d099aef68
abchiarelli <121977175+abchiarelli@users.noreply.github.com>
2023-06-01 20:15:50 -0300

    Create Usuario.java

---
+++ VendaMercadorias/src/entidade/Usuario.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidade;
+
+/**
+ *
+ * @author artur
+ */
+public class Usuario {
+    int id;
+    String login;
+    String senha;
+    char situacao;
+
+    public Usuario(String login, String senha) {
+        this.login = login;
+        this.senha = senha;
+    }
+    
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public char getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(char situacao) {
+        this.situacao = situacao;
+    }
+    
+    
+}
