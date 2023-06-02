abf94cd1a83faa8b97cafbe303fd6524c9dcd555
abchiarelli <121977175+abchiarelli@users.noreply.github.com>
2023-06-02 16:27:20 -0300

    Update Formatacao.java
    Implementada mensagem de retorno para CampoComErro

--- VendaMercadorias/src/apoio/Formatacao.java
+++ VendaMercadorias/src/apoio/Formatacao.java
@@ -145,4 +145,8 @@ public class Formatacao {
     public static Color colorNeutral() {
         return Color.WHITE;
     }
+    
+    public static String mensagemErroPreenchimento() {
+        return "Há campo(s) com erro de preenchimento.";
+    }
 }
