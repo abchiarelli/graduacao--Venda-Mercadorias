ad3d742e59fe947ccecdabc2c2af669fbd41f59d
abchiarelli <121977175+abchiarelli@users.noreply.github.com>
2023-06-12 22:09:06 -0300

    Create ItemPedido.java

---
+++ VendaMercadorias/src/entidade/ItemPedido.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidade;
+
+/**
+ *
+ * @author artur
+ */
+public class ItemPedido {
+    int idPedido;
+    int idProduto;
+    double valor;
+    double quantidade;
+
+    public ItemPedido(int idPedido, int idProduto, double valor, double quantidade) {
+        this.idPedido = idPedido;
+        this.idProduto = idProduto;
+        this.valor = valor;
+        this.quantidade = quantidade;
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+    
+}
