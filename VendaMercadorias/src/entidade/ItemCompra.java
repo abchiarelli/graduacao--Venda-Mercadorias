216f91ffed9800e6b50fe025fe0c0c55d53e1609
abchiarelli <121977175+abchiarelli@users.noreply.github.com>
2023-06-21 15:23:06 -0300

    Create ItemCompra.java

---
+++ VendaMercadorias/src/entidade/ItemCompra.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidade;
+
+/**
+ *
+ * @author artur
+ */
+public class ItemCompra {
+    private int idProduto;
+    private int idCompra;
+    private double quantidade;
+    private double valor;
+
+    public ItemCompra(int idProduto, int idCompra, double quantidade, double valor) {
+        this.idProduto = idProduto;
+        this.idCompra = idCompra;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public int getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(int idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+    
+}
