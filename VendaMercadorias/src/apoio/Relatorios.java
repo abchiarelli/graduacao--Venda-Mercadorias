41db80f6fb2558c4b8fa7d05c1305d01186f7706
abchiarelli <121977175+abchiarelli@users.noreply.github.com>
2023-06-22 19:18:31 -0300

    Update Relatorios.java
    implementado métodos gerarRelatorioData() e gerarCSV()

--- VendaMercadorias/src/apoio/Relatorios.java
+++ VendaMercadorias/src/apoio/Relatorios.java
@@ -4,6 +4,8 @@
  */
 package apoio;
 
+import com.opencsv.CSVWriter;
+import java.io.FileWriter;
 import java.util.HashMap;
 import java.util.Map;
 import javax.swing.JOptionPane;
@@ -12,13 +14,15 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.view.JasperViewer;
+import java.sql.ResultSet;
 
 /**
  *
  * @author artur
  */
 public class Relatorios {
-    public void gerarRelatorio(String caminho) {
+
+    public void gerarListagem(String caminho) {
         try {
             JasperReport relatorio = JasperCompileManager.compileReport(getClass().getResourceAsStream(caminho));
             Map parametros = new HashMap();
@@ -27,6 +31,33 @@ public class Relatorios {
         } catch (Exception e) {
             System.out.println("Erro ao gerar relatório: " + e);
             JOptionPane.showMessageDialog(null, "Houve algum problema ao gerar relatório.");
+        }
+    }
+
+    public void gerarRelatorioData(String caminho, String dataDe, String dataAte) {
+        try {
+            JasperReport relatorio = JasperCompileManager.compileReport(getClass().getResourceAsStream(caminho));
+
+            Map parametros = new HashMap();
+            parametros.put("dataDe", dataDe);
+            parametros.put("dataAte", dataAte);
+
+            JasperPrint impressao = JasperFillManager.fillReport(relatorio, parametros, ConexaoBD.getInstancia().getConexao());
+            JasperViewer.viewReport(impressao, false);
+        } catch (Exception e) {
+            System.out.println("Erro ao gerar relatório: " + e);
+            JOptionPane.showMessageDialog(null, "Houve algum problema ao gerar relatório.");
+        }
+    }
+
+    public static void gerarCSV(ResultSet rs, String localArquivo) {
+        try {
+            CSVWriter writer = new CSVWriter(new FileWriter(localArquivo), ';', '\0', '\0', "\n");
+            writer.writeAll(rs, true);
+            writer.close();
+            JOptionPane.showMessageDialog(null, "Arquivo CSV (.csv) criado com sucesso.");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao criar arquivo CSV (.csv).");
         }
     }
 }
